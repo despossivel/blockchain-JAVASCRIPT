745784edaca0679dab1cf239ae13c60d70ee13cc
matheus <matheus@ti>
2019-06-11 16:08:09 -0300

    primeiro commit

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const Blockchain = require('./blockchain')
+//https://medium.com/tableless/como-implementar-o-blockchain-em-javascript-com-menos-de-80-linhas-c2ff4852ee13
+
 
 const blockchain = new Blockchain()
 blockchain.addBlock({ amount: 4 })
